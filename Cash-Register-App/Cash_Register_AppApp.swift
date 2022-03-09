fd8243c9991e34434bb0c9c87b58bd1fe77ca582
RohitP123 <90308603@ep-student.org>
2022-03-09 12:28:56 -0600

    Initial Commit

---
+++ Cash-Register-App/Cash_Register_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cash_Register_AppApp.swift
+//  Cash-Register-App
+//
+//  Created by 90308603 on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Cash_Register_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cash-Register-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cash-Register-App
+//
+//  Created by 90308603 on 3/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
